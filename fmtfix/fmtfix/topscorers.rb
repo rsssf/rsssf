196ba18f38cbabeab76407d26fe27866ef0f81db
Gerald Bauer <gerald.bauer@gmail.com>
2026-04-18 15:41:49 +0200

    up

--- fmtfix/fmtfix/topscorers.rb
+++ fmtfix/fmtfix/topscorers.rb
@@ -2,69 +2,19 @@
 ##
 ## process/handle Topscoreres: ... to first blank line (\n\n)
 
-=begin
 
-Topscorers:
-16 Diego Cervero                - UD Logroñés
-16 Jon Altuna                   - Éibar
-15 Iker Torre                   - Zamora
-13 Urko Vera                    - Lemona
-13 Gorka Brit                   - Real Unión
-12 "Javi" Fernandez Anunziatta  - Osasuna B
-12 "Jito" Juan Silvestre        - Alavés
-12 Alejandro Suárez             - Palencia
+## e.g.
+##  topscorer, topscorers
+##  top scorer, top scorers
+##  scorer, scorers
 
--or-
-
-Topscorers
-
-41 "Cristiano Ronaldo" dos Santos Aveiro - Real Madrid
-31 Lionel Messi                          - Barcelona
-20 Sergio Agüero                         - Atlético
-20 Alvaro Negredo                        - Sevilla
-18 Fernando  Llorente                    - Athletic
-18 "Rossi" - Giuseppe Scurto             - Villarreal
-18 Roberto Soldado                       - Valencia
-18 David Villa                           - Barcelona
-
--or   "inline"
-
-Topscorers: Rudi BRUNNENMEIER (TSV) - 24, Timo KONIETZKA
-(Dortmund) - 22, Christian MÜLLER (Köln) - 18
-
-Topscorers: Timo KONIETZKA (TSV) - 26, Arnold SCHÜTZ
-(Werder) - 20, Hannes LÖHR (Köln) and Peter GROSSER
-(TSV) - 18
-
-Topscorers: Gerd MÜLLER (Bayern) - 40, Klaus FISCHER (Schalke) and
-Hans WALITZA (Bochum) - 22, Ferdinand KELLER (Hannover) - 20
-
--or-  ### singular
-
-Topscorer: Danilo Pitbull (Guarany), 11 goals.
-
-Topscorer: Bruno Rangel (Paysandu), 8 goals.
-     Cascata (ABC) and Felipe Mamão (Águia), 5 goals.
-
--or-    top scorer/top scorers
-
-Top scorers:
-Jurcevic        (Austria Salzburg)         14 (2p)
-Pfeifenberger   (Austria Salzburg)         14 (1p)
-
-Top scorer:
-
-Stumpf          (Linz)                     21
-
-
-
-=end
-
-
-
-TOPSCORERS_RE = %r{^     [ ]* Top [ ]? scorers?  
-                           :?           ## note - optional colon
-                         [ ]*
+TOPSCORERS_RE = %r{^     [ ]* 
+                        (?<header> 
+                           (?: top [ ]?)?  ## note - optional top
+                              scorers?      ## singular or plural 
+                         )
+                          (?: [ ]* :)?    ## note - optional colon
+                           [ ]*
                           \n{0,2}       ## note - optional leading blank line!!
 
                         .*?             ## non-greedy - match everything until
--- fmtfix/test_topscorers.rb
+++ fmtfix/test_topscorers.rb
@@ -1,6 +1,6 @@
 ###
 ##  to run tests use
-##   $ ruby  fmtfix/test_tables.rb
+##   $ ruby  fmtfix/test_topscorers.rb
 
 
 ## minitest setup
@@ -11,12 +11,12 @@ require_relative  'fmtfix'
 
 
 
-class TestTables< Minitest::Test
+class TestScorers< Minitest::Test
 
-def test_headers
+def test_scorers
 
   ## norm text
-   txt = read_text( "./fmtfix/test_tables.txt" )
+   txt = read_text( "./fmtfix/test_topscorers.txt" )
    txt = txt.gsub( "\t", '  ' )
    txt = txt.gsub( "\r\n", "\n" )
    ## add smart quotes and unicode minus/hyphen etc.
@@ -25,41 +25,41 @@ def test_headers
    ## not - split in sections  with §
    ##   strips leading & trailing newlines
    ##    auto-adds a trailing newline
-   tables = txt.split( %r{^
+   sects = txt.split( %r{^
                             [ ]* § [ ]*
                               \n{1,2}
                          }x )
 
 
-    tables.each do |table|
+    sects.each do |sect|
       
-        m = TABLE_RE.match( table )
+        m = TOPSCORERS_RE.match( sect )
         if m && m.offset(0)[0] == 0
 
             ## check if leftover after table
-            footer = table[m.offset(0)[1]..-1]
+            footer = sect[m.offset(0)[1]..-1]
             if /\A[ \n]+\z/.match( footer )
-              puts "  OK  table >#{m[:header]}<"
+              puts "  OK  topscorers >#{m[:header]}<"
               assert true
             else
-              puts "!! ERROR - matching table BUT has left over footer; starting at #{m.offset(0)[1]}"
+              puts "!! ERROR - matching topscorers BUT has left over footer; starting at #{m.offset(0)[1]}"
               pp footer
               puts "---"
-              pp table
+              pp sect
               assert false
             end
         elsif m 
             puts "!! ERROR - match NOT anchored to begin-of-line; starting at #{m.offset(0)[0]}" 
-            pp table
+            pp sect
             assert false
         else 
             puts "!! ERROR - no match"
-            pp table               
+            pp sect               
             assert false
         end
   end
-end  # method test_headers
-end  # class TestTables
+end  # method test_scorers
+end  # class TestScorers
 
 
 
